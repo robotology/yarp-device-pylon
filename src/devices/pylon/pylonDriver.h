1a8da950e3445b811050643ac6dad7412ef36296
Nicogene <nicolo.genesio@iit.it>
2022-08-01 15:32:51 +0200

    Add first (empty) pylon device

---
+++ src/devices/pylon/pylonDriver.h
@@ -0,0 +1,98 @@
+/*
+ * Copyright (C) 2006-2022 Istituto Italiano di Tecnologia (IIT)
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms of the
+ * BSD-3-Clause license. See the accompanying LICENSE file for details.
+ */
+
+#ifndef PYLON_DRIVER_H
+#define PYLON_DRIVER_H
+
+#include <iostream>
+#include <cstring>
+#include <map>
+#include <mutex>
+
+#include <yarp/dev/DeviceDriver.h>
+#include <yarp/dev/IFrameGrabberControls.h>
+#include <yarp/dev/IFrameGrabberImage.h>
+#include <yarp/dev/IRgbVisualParams.h>
+#include <yarp/os/PeriodicThread.h>
+#include <yarp/sig/all.h>
+#include <yarp/sig/Matrix.h>
+#include <yarp/os/Stamp.h>
+#include <pylon/PylonIncludes.h>
+
+
+class pylonDriver :
+        public yarp::dev::DeviceDriver,
+        public yarp::dev::IFrameGrabberControls,
+        public yarp::dev::IFrameGrabberImage,
+        public yarp::dev::IRgbVisualParams
+{
+private:
+    typedef yarp::sig::ImageOf<yarp::sig::PixelFloat> depthImage;
+    typedef yarp::os::Stamp                           Stamp;
+    typedef yarp::os::Property                        Property;
+    typedef yarp::sig::FlexImage                      FlexImage;
+
+
+public:
+    pylonDriver();
+    ~pylonDriver() override = default;
+
+    // DeviceDriver
+    bool open(yarp::os::Searchable& config) override;
+    bool close() override;
+
+    // IRgbVisualParams
+    int    getRgbHeight() override;
+    int    getRgbWidth() override;
+    bool   getRgbSupportedConfigurations(yarp::sig::VectorOf<yarp::dev::CameraConfig> &configurations) override;
+    bool   getRgbResolution(int &width, int &height) override;
+    bool   setRgbResolution(int width, int height) override;
+    bool   getRgbFOV(double& horizontalFov, double& verticalFov) override;
+    bool   setRgbFOV(double horizontalFov, double verticalFov) override;
+    bool   getRgbMirroring(bool& mirror) override;
+    bool   setRgbMirroring(bool mirror) override;
+    bool   getRgbIntrinsicParam(Property& intrinsic) override;
+
+
+    //IFrameGrabberControls
+    bool   getCameraDescription(CameraDescriptor *camera) override;
+    bool   hasFeature(int feature, bool*   hasFeature) override;
+    bool   setFeature(int feature, double  value) override;
+    bool   getFeature(int feature, double* value) override;
+    bool   setFeature(int feature, double  value1,  double  value2) override;
+    bool   getFeature(int feature, double* value1,  double* value2) override;
+    bool   hasOnOff(  int feature, bool*   HasOnOff) override;
+    bool   setActive( int feature, bool    onoff) override;
+    bool   getActive( int feature, bool*   isActive) override;
+    bool   hasAuto(   int feature, bool*   hasAuto) override;
+    bool   hasManual( int feature, bool*   hasManual) override;
+    bool   hasOnePush(int feature, bool*   hasOnePush) override;
+    bool   setMode(   int feature, FeatureMode mode) override;
+    bool   getMode(   int feature, FeatureMode *mode) override;
+    bool   setOnePush(int feature) override;
+
+    //IFrameGrabberImage
+    bool getImage(yarp::sig::ImageOf<yarp::sig::PixelRgb>& image) override;
+    int height() const override;
+    int width() const override;
+
+private:
+    //method
+    //inline bool setParams();
+    bool        setFramerate(const int _fps);
+
+
+    mutable std::mutex m_mutex;
+
+    yarp::os::Stamp m_rgb_stamp;
+    mutable std::string m_lastError;
+    bool m_verbose;
+    bool m_initialized;
+    int m_fps;
+};
+#endif // PYLON_DRIVER_H
